9563d1cd137474b4d19b5c5fbc74dcad2786e8c5
Batata <andredalbosco2020@gmail.com>
2021-04-05 20:49:55 -0300

    added linkedin oauth to settings.py

--- back/cee/settings.py
+++ back/cee/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     # 'allauth.socialaccount',
     # 'allauth.socialaccount.providers.google',
     # 'allauth.socialaccount.providers.facebook',
+    # 'allauth.socialaccount.providers.linkedin_oauth2',
 ]
 
 MIDDLEWARE = [
