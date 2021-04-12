07814f6dbea8bc09539797b96497767d0264f6b7
Batata <andredalbosco2020@gmail.com>
2021-04-12 15:59:34 -0300

    add some app to settings.py

--- back/cee/settings.py
+++ back/cee/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -38,6 +39,10 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'users',
+    'companies',
+    'events',
+    'vacancies',
+    'rest_framework',
     # 'django.contrib.sites',
     # 'allauth',
     # 'allauth.account',
@@ -148,3 +153,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 
 AUTH_USER_MODEL = 'users.CustomUser'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+MEDIA_URL = '/media/'
