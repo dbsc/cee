2575371c4fb57d9f090d2516f0944a5e1fec4582
Batata <andredalbosco2020@gmail.com>
2021-04-12 16:00:09 -0300

    add vacancies app on main urls.py

--- back/cee/urls.py
+++ back/cee/urls.py
@@ -18,5 +18,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('vacancies/', include('vacancies.urls')),
     # path('accounts/', include('allauth.urls')),
 ]
