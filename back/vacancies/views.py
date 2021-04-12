d72f237bd7731a64fcb321dbced52c2d337fdd2c
Batata <andredalbosco2020@gmail.com>
2021-04-12 16:04:02 -0300

    vacancies views initial setup

--- back/vacancies/views.py
+++ back/vacancies/views.py
@@ -1,3 +1,23 @@
-from django.shortcuts import render
+# from rest_framework import generics
+from rest_framework import viewsets
 
-# Create your views here.
+from .models import Vacancy, Requirement, Field
+from .serializers import (
+    VacancySerializer, RequirementSerializer, FieldSerializer
+)
+
+
+# class VacancyList(generics.ListCreateAPIView):
+#     queryset = Vacancy.objects.all()
+#     serializer_class = VacancySerializer
+
+
+
+# class VacancyView(generics.RetrieveUpdateDestroyAPIView):
+#     queryset = Vacancy.objects.all()
+#     serializer_class = VacancySerializer
+
+
+class VacancyViewSet(viewsets.ModelViewSet):
+    queryset = Vacancy.objects.all()
+    serializer_class = VacancySerializer
