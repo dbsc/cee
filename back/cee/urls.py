5cdd52a1fe2d4b97d86580107109bf1422c397b0
Batata <andredalbosco2020@gmail.com>
2021-04-17 17:45:18 -0300

    include auth routes and static media

--- back/cee/urls.py
+++ back/cee/urls.py
@@ -15,9 +15,15 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('vacancies/', include('vacancies.urls')),
     # path('accounts/', include('allauth.urls')),
-]
+    path('auth/', include('rest_framework.urls')),
+    path('rest-auth/', include('rest_auth.urls')),
+    path('rest-auth/registration/', include('rest_auth.registration.urls')),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
