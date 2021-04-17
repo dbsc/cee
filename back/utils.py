4c4777c5919afed5279f0f8102a334d0a2b37a9a
Batata <andredalbosco2020@gmail.com>
2021-04-17 18:23:36 -0300

    added a unique file name generator and file size validator

---
+++ back/utils.py
@@ -0,0 +1,40 @@
+from django.core.exceptions import ValidationError
+from django.utils.deconstruct import deconstructible
+from django.utils.text import slugify
+from os.path import splitext, join
+from uuid import uuid4
+
+
+@deconstructible
+class UniqueFileName(object):
+
+    def __init__(self, path):
+        self.path = join(path, "%s%s")
+
+    def __call__(self, instance, filename):
+        name = slugify(str(instance)) + uuid4()
+        extension = splitext(filename)[1]
+        return self.path % (name, extension)
+
+
+@deconstructible
+class FileSizeValidator(object):
+
+    def __init__(self, max_size):
+        self.max_size = mb_to_bytes(max_size)
+
+    def __call__(self, size):
+        if size > self.max_size:
+            raise ValidationError("File size cannot exceed %.2f", self.max_size_in_mb)
+
+    @property
+    def max_size_in_mb(self):
+        return bytes_to_mb(self.max_size)
+
+
+def mb_to_bytes(size):
+    return 1024 * 1024 * size
+
+
+def bytes_to_mb(size):
+    return size / (1024 * 1024)
