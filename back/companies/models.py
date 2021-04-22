5f735477990b6d0f9c09907ec3bef820ee260da6
Batata <andredalbosco2020@gmail.com>
2021-04-22 20:47:39 -0300

    added serializers and views

--- back/companies/models.py
+++ back/companies/models.py
@@ -1,13 +1,15 @@
 from django.db import models
-from string import whitespace
-
-
-def upload_location(instance, filename):
-    _, extension = filename.split('.')
-    name, _ = instance.name.replace(whitespace, '_')
-    return 'companies/%s.%s' % (name, extension)
+from utils import FileSizeValidator, UniqueFileName
 
 
 class Company(models.Model):
     name = models.CharField(max_length=150)
-    image = models.ImageField(upload_to=upload_location, blank=True, null=True)
+    logo = models.ImageField(
+        upload_to=UniqueFileName('companies/logos'),
+        validators=[FileSizeValidator(2)],
+        blank=True,
+        null=True
+    )
+
+    def __str__(self):
+        return self.name
--- back/companies/views.py
+++ back/companies/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import permissions
+from rest_framework import viewsets
+from .models import Company
+from .serializers import CompanySerializer
 
-# Create your views here.
+
+class CompanyViewSet(viewsets.ModelViewSet):
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+    queryset = Company.objects.all()
+    serializer_class = CompanySerializer
