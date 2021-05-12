5363882d697e18d4ccdbda5065737a4f50878994
Batata <andredalbosco2020@gmail.com>
2021-05-12 14:23:14 -0300

    added Event model

--- back/events/models.py
+++ back/events/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from companies.models import Company
+from vacancies.models import Field, Tag
+from datetime import timedelta
 
-# Create your models here.
+
+class Event(models.Model):
+    title = models.CharField(max_length=250)
+    description = models.TextField()
+    datetime = models.DateTimeField()
+    duration = models.DurationField(default=timedelta(hours=1))
+    image = models.ImageField()
+    link = models.URLField()
+    field = models.ForeignKey(
+        Field,
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True
+    )
+    company = models.ForeignKey(
+        Company,
+        on_delete=models.PROTECT,
+        blank=True,
+        null=True
+    )
+    tags = models.ManyToManyField(
+        Tag,
+        blank=True
+    )
