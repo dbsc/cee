a516cc9bf41e09dfaf2ef68cf7eb079723d147f7
Batata <andredalbosco2020@gmail.com>
2021-05-12 14:30:22 -0300

    update urls.py

--- back/cee/urls.py
+++ back/cee/urls.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from django.urls import path, include, re_path
+from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
 
@@ -11,7 +11,7 @@ schema_view = get_schema_view(
     openapi.Info(
         title="CEE API",
         default_version="v1",
-        description="Test Description"
+        description="API for the CEE Website",
     ),
     public=True,
     permission_classes=[permissions.AllowAny]
@@ -21,11 +21,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('vacancies/', include('vacancies.urls')),
     path('companies/', include('companies.urls')),
+    path('events/', include('events.urls')),
     path('auth/', include('rest_framework.urls')),
     path('rest-auth/', include('rest_auth.urls')),
     path('accounts/', include('allauth.urls')),
     path('rest-auth/registration/', include('rest_auth.registration.urls')),
-    re_path(r'^swagger(?P<format>\.json|\.yaml)$', schema_view.without_ui(cache_timeout=0), name='schema-json'),
-    re_path(r'^swagger/$', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
-    re_path(r'^redoc/$', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
