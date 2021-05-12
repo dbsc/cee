bf3ca3067e8b656f6ec08d42722e7676660adf6a
Batata <andredalbosco2020@gmail.com>
2021-05-12 14:22:11 -0300

    updated urls.py

--- back/vacancies/urls.py
+++ back/vacancies/urls.py
@@ -3,18 +3,17 @@ from rest_framework import routers
 from .views import (
     FieldViewSet, PositionViewSet, RequirementViewSet,
     SimpleVacancyViewSet, VacancyViewSet, TagViewSet,
-    SkillViewSet, ResponsabilityViewSet
+    ResponsibilityViewSet
 )
 
 router = routers.DefaultRouter()
-router.register(r'vacancies', VacancyViewSet)
+router.register(r'vacancies', VacancyViewSet, basename='vacancies')
 router.register(r'simplevacancies', SimpleVacancyViewSet)
 router.register(r'requirements', RequirementViewSet)
 router.register(r'fields', FieldViewSet)
 router.register(r'positions', PositionViewSet)
 router.register(r'tags', TagViewSet)
-router.register(r'skills', SkillViewSet)
-router.register(r'responsabilities', ResponsabilityViewSet)
+router.register(r'responsibilities', ResponsibilityViewSet)
 
 urlpatterns = [
     path('', include(router.urls))
