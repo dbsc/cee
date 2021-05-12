db55f9157ef88cefab683b9d93edb99f4be93e00
Batata <andredalbosco2020@gmail.com>
2021-05-12 14:26:03 -0300

    modified user fields

--- back/users/models.py
+++ back/users/models.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.validators import UnicodeUsernameValidator
 from django.contrib import auth
 from django.core.mail import send_mail
+from django.core.validators import EmailValidator
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
@@ -14,27 +15,24 @@ from datetime import date
 class CustomUserManager(BaseUserManager):
     use_in_migrations = True
 
-    def _create_user(self, username, email, password, **extra_fields):
+    def _create_user(self, email, password, **extra_fields):
         """
-        Create and save a user with the given username, email, and password.
+        Create and save a user with the given email and password.
         """
         if not email:
             raise ValueError('The given email must be set')
-        if not username:
-            raise ValueError('The given username must be set')
         email = self.normalize_email(email)
-        username = self.model.normalize_username(username)
-        user = self.model(username=username, email=email, **extra_fields)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save(using=self._db)
         return user
 
-    def create_user(self, username, email=None, password=None, **extra_fields):
+    def create_user(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', False)
         extra_fields.setdefault('is_superuser', False)
-        return self._create_user(username, email, password, **extra_fields)
+        return self._create_user(email, password, **extra_fields)
 
-    def create_superuser(self, username, email=None, password=None, **extra_fields):
+    def create_superuser(self, email, password=None, **extra_fields):
         extra_fields.setdefault('is_staff', True)
         extra_fields.setdefault('is_superuser', True)
 
@@ -43,7 +41,7 @@ class CustomUserManager(BaseUserManager):
         if extra_fields.get('is_superuser') is not True:
             raise ValueError('Superuser must have is_superuser=True.')
 
-        return self._create_user(username, email, password, **extra_fields)
+        return self._create_user(email, password, **extra_fields)
 
     def with_perm(self, perm, is_active=True, include_superusers=True, backend=None, obj=None):
         if backend is None:
@@ -74,10 +72,7 @@ class CustomUserManager(BaseUserManager):
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
     """
-    An abstract base class implementing a fully featured User model with
-    admin-compliant permissions.
-
-    Username and password are required. Other fields are optional.
+    Email and password are required. Other fields are optional.
     """
     CLASS_YEAR_CHOICES = [('T%02d' % year, 'T%02d' % year) for year in
                           chain(range(54, 100), range(0, 2000 - date.today().year + 4))]
@@ -92,9 +87,18 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
             'unique': _("A user with that username already exists."),
         },
     )
-    first_name = models.CharField(_('first name'), max_length=150, blank=True)
-    last_name = models.CharField(_('last name'), max_length=150, blank=True)
-    email = models.EmailField(_('email address'), blank=True)
+    # first_name = models.CharField(_('first name'), max_length=150, blank=True)
+    # last_name = models.CharField(_('last name'), max_length=150, blank=True)
+    email_validator = EmailValidator()
+    email = models.EmailField(
+        _('email address'),
+        unique=True,
+        help_text=_('Required.'),
+        validators=[email_validator],
+        error_messages={
+            'unique': _("A user with that email already exists"),
+        },
+    )
     class_year = models.CharField(
         max_length=3,
         choices=CLASS_YEAR_CHOICES,
@@ -116,9 +120,12 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     objects = CustomUserManager()
 
+    # EMAIL_FIELD = 'email'
+    # USERNAME_FIELD = 'username'
+    # REQUIRED_FIELDS = ['email']
+
+    USERNAME_FIELD = 'email'
     EMAIL_FIELD = 'email'
-    USERNAME_FIELD = 'username'
-    REQUIRED_FIELDS = ['email']
 
     def __str__(self):
         return self.email
@@ -126,22 +133,21 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
     class Meta:
         verbose_name = _('user')
         verbose_name_plural = _('users')
-        # abstract = True
 
     def clean(self):
         super().clean()
         self.email = self.__class__.objects.normalize_email(self.email)
 
-    def get_full_name(self):
-        """
-        Return the first_name plus the last_name, with a space in between.
-        """
-        full_name = '%s %s' % (self.first_name, self.last_name)
-        return full_name.strip()
+    # def get_full_name(self):
+    #     """
+    #     Return the first_name plus the last_name, with a space in between.
+    #     """
+    #     full_name = '%s %s' % (self.first_name, self.last_name)
+    #     return full_name.strip()
 
-    def get_short_name(self):
-        """Return the short name for the user."""
-        return self.first_name
+    # def get_short_name(self):
+    #     """Return the short name for the user."""
+    #     return self.first_name
 
     def email_user(self, subject, message, from_email=None, **kwargs):
         """Send an email to this user."""
