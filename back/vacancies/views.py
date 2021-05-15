07f0134db3b41e67860e2772c01717fa8287c937
Batata <andredalbosco2020@gmail.com>
2021-05-15 14:47:30 -0300

    added some actions to vacancy viewset

--- back/vacancies/views.py
+++ back/vacancies/views.py
@@ -1,3 +1,4 @@
+from rest_framework.status import HTTP_201_CREATED, HTTP_400_BAD_REQUEST
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
 from rest_framework.viewsets import ModelViewSet
 from rest_framework.decorators import action
@@ -12,11 +13,14 @@ from .serializers import (
     FieldSerializer, PositionSerializer, TagSerializer,
     ResponsibilitySerializer
 )
+from .fields import RequirementNestedField
+
 from companies.serializers import CompanySerializer
 
 
 class SimpleVacancyViewSet(ModelViewSet):
-    # permission_classes = (IsAuthenticatedOrReadOnly,)
+    """Returns a list of the all the SimpleVacancy objects."""
+    permission_classes = (IsAuthenticatedOrReadOnly,)
     queryset = SimpleVacancy.objects.all()
     serializer_class = SimpleVacancySerializer
 
@@ -28,6 +32,7 @@ class VacancyViewSet(ModelViewSet):
 
     @action(detail=True)
     def responsibilities(self, request, pk=None):
+        """List all the responsibilities regarding the vacancy"""
         vacancy = self.get_object()
         responsibilities = vacancy.responsibilities.all()
         serializer = ResponsibilitySerializer(responsibilities, many=True, fields=['id', 'description'])
@@ -35,17 +40,27 @@ class VacancyViewSet(ModelViewSet):
 
     @action(detail=True)
     def company(self, request, pk=None):
+        """Detail of vacancy's company"""
         vacancy = self.get_object()
         company = vacancy.company
         serializer = CompanySerializer(company)
         return Response(serializer.data)
 
-    @action(detail=True)
+    @action(detail=True, methods=['GET', 'POST'])
     def requirements(self, request, pk=None):
-        vacancy = self.get_object()
-        requirements = vacancy.requirements.alL()
-        serializer = RequirementSerializer(requirements, many=True)
-        return Response(serializer.data)
+        """List all the vacancy's requirements, or create new requirements"""
+        if request.method == 'GET':
+            vacancy = self.get_object()
+            requirements = vacancy.requirements.all()
+            serializer = RequirementSerializer(requirements, many=True)
+            return Response(serializer.data)
+        elif request.method == 'POST':
+            vacancy = self.get_object()
+            serializer = RequirementNestedField(data=request.data)
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data, status=HTTP_201_CREATED)
+            return Response(serializer.error, status=HTTP_400_BAD_REQUEST)
 
     @action(detail=True)
     def tags(self, request, pk=None):
@@ -61,13 +76,12 @@ class VacancyViewSet(ModelViewSet):
         serializer = PositionSerializer(position)
         return Response(serializer.data)
 
-    @action(detail=True)
+    @action(detail=True, methods=[])
     def field(self, request, pk=None):
         vacancy = self.get_object()
         field = vacancy.field
         serializer = FieldSerializer(field)
         return Response(serializer.data)
-
 
 
 class RequirementViewSet(ModelViewSet):
